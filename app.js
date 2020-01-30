ed4339b4d2e486c49cf3aa8fd32230925caadae7
Anna Panas <annapanas0906@gmail.com>
2020-01-29 21:10:37 -0500

    added inquirer to prompt user for their information

--- app.js
+++ app.js
@@ -15,39 +15,140 @@ class Employee {
 
     getEmail() { }
 
-    getRole() { }
+    getRole() {
+        return "Employee"
+    }
 }
 
-// Manager extends Employee
+
 class Manager extends Employee {
     constructor(officeNumber) {
-        super(name, id, title) // manager gets it's parent's (Employee) properties in addition to officeNumber
+        super(name, id, title) // accessing parent's properties
         this.officeNumber = officeNumber;
     }
 
-    getRole() { }
+    getRole() {
+        return "Manager"
+    }
 }
 
-// Engineer extends Employee
 
 class Engineer extends Employee {
     constructor(Github) {
-        super(name, id, title); // Engineer gets it's parent's (Employee) properties in addition to Github username
+        super(name, id, title); // accessing parent's properties
         this.Github = Github;
     }
 
     getGithub() { }
 
-    getRole() { }
+    getRole() {
+        return "Engineer"
+    }
 }
 
-// Intern extends Employee
 class Intern extends Employee {
     constructor(school) {
-        super(name, id, title); // Intern gets it's parent's (Employee) properties in addition to school
+        super(name, id, title); // accessing parent's properties
         this.school = school;
     }
     getSchool() { }
 
-    getRole() { }
+    getRole() {
+        return "Intern"
+    }
 }
+
+// using inquirer to get user input
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is your manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your manager's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your manager's email?"
+    },
+    {
+        type: "input",
+        name: "office room number",
+        message: "What is your manager's office room number?"
+    },
+    {
+        type: "list",
+        message: "What type of team member would you like to add?",
+        name: "team member",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is your engineer's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your engineer's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your engineer's email?"
+    },
+    {
+        type: "input",
+        name: "Github username",
+        message: "What is your engineer's Github username?"
+    },
+    {
+        type: "list",
+        message: "What type of team member would you like to add?",
+        name: "team member",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is your intern's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your intern's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your intern's email?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is your intern's school?"
+    },
+    {
+        type: "list",
+        message: "What type of team member would you like to add?",
+        name: "team member",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    }
+])
